303f76fe8f9fea74eb7ea9fabcb888fb3af4c63a
Fabio Henrique <Farabio973@hotmail.com>
2024-01-01 22:39:41 -0300

    First version, without style, only JS and Html base

---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+const limit = 20;
+const offset = 0;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+const pokeList = document.querySelector("#pokelist");
+const pokeElement = document.querySelector("#element");
+
+fetch(url)
+    .then(response => response.json())
+    .then(listPokemon => listPokemon.results)
+    .then(pokemons => pokemons.map(getDetailsPokemons))
+    .then(pokemonsDetails => Promise.all(pokemonsDetails))
+    .then(pokemons =>  pokeList.innerHTML += pokemons.map(convertToHtml).join(''))
+
+
+
+function convertToHtml(pokemon) {
+    console.log(pokemon)
+    return `<li class="card ${pokemon.types[0].type.name}">
+                <div class="info-pokemon">
+                    <div class="name">
+                        <p>${pokemon.name}</p>
+                        <p>#${pokemon.id}</p>
+                    </div>
+                    <ul id="element ">
+                        ${pokemon.types.map((type) => `<li class="type">${type.type.name}</li>`).join('')}
+                    </ul>
+                </div>
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name} title="${pokemon.name}">
+            </li>`
+}
+
+async function getDetailsPokemons(pokemon) {
+    const listDetails = await fetch(pokemon.url);
+    return await listDetails.json();
+}
